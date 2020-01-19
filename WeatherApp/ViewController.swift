1c29a62988736f54c2309cf888e4bde50fb9b721
Aman Gupta <aman.gupta@nagarro.com>
2020-01-19 14:02:17 +0530

    Initial commit - create new project

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Aman Gupta on 19/01/20.
+//  Copyright © 2020 Aman Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
